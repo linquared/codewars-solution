577a77249b938fb46d24a9b5f6cc31a2a053ec42
“Lin <99622876+linsquared@users.noreply.github.com>
2022-06-11 12:15:44 -0400

    solution 9 completed

---
+++ js/solution009.js
@@ -0,0 +1,8 @@
+function makeNegative(num) {
+    return num > 0 ? -num : num
+}
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
