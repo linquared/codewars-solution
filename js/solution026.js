324e0b15ffa55375ea74a19b7c7f97bb6bffcc04
“Lin <99622876+linsquared@users.noreply.github.com>
2022-06-30 23:05:10 -0400

    26

---
+++ js/solution026.js
@@ -0,0 +1,11 @@
+// 8lyu Let's play! You have to return which player won! In case of a draw return Draw!.
+function rps (p1, p2){
+    if(p1 === 'scissors' && p2 === 'paper' || p1 === 'rock' && p2 === 'scissors' || p1 === 'paper' && p2 === 'rock'){
+      return 'Player 1 won!'
+    }else if(p1 === p2){
+      return 'Draw!'
+    }else{
+      return 'Player 2 won!'
+    }
+  }
+  
