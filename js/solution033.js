b8fb529b6a5685a6804c49c437698e60ffeed8bd
“Lin <99622876+linsquared@users.noreply.github.com>
2022-07-08 10:03:23 -0400

    33

---
+++ js/solution033.js
@@ -0,0 +1,11 @@
+
+// 8kyu Now you have to write a function that takes an argument and returns the square of it.
+
+function square(x){
+  return x*x
+}
+
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+function sayHello(name) {
+  return `Hello, ${name}`
+}
