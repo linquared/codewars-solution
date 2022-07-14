6fc301f6190ed879f06f1e7ab6694b61a34fb804
“Lin <99622876+linsquared@users.noreply.github.com>
2022-07-14 08:57:35 -0700

    39

---
+++ js/solution039.js
@@ -0,0 +1,13 @@
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+function countBy(x, n) {
+    let z = [];
+  
+      for(let i = x; i <= (x * n); i+=x ){
+        z.push(i)
+      }
+    return z;
+  }
+  
+  
+  
