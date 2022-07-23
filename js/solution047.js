893285684390ba9f8e570bfa56786971e72738fe
“Lin <99622876+linsquared@users.noreply.github.com>
2022-07-23 08:58:17 -0400

    47

---
+++ js/solution047.js
@@ -0,0 +1,5 @@
+// (array 7kyu) Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+function evenNumbers(array,number) {
+    return array.filter(num => num % 2 == 0).slice(-number)
+    
+  }
