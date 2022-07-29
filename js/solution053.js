934cd8369cd89b696999bf22de34e8fe321729b1
“Lin <99622876+linsquared@users.noreply.github.com>
2022-07-29 11:02:13 -0400

    53

---
+++ js/solution053.js
@@ -0,0 +1,6 @@
+
+// 7kyu - Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+function toNumberArray(stringarray){
+    return stringarray.map(num => Number(num))
+  }
+  
