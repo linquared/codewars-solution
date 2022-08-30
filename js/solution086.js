85a5b532ddf2503846f9652b3c2aff6f7dd4e645
linsquared <lin2013lin@gmail.com>
2022-08-29 22:29:12 -0400

    86

---
+++ js/solution086.js
@@ -0,0 +1,8 @@
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+function arr(n){
+  let array = []
+  for (let i = 0; i < n; i++){
+    array.push(i)
+  }
+  return array
+}
