f7fe9940ab6d2d09363cc2251208d87ff30d2b75
linsquared <lin2013lin@gmail.com>
2022-10-04 14:22:22 -0400

    119

---
+++ js/solution119.js
@@ -0,0 +1,4 @@
+// 8kyu- Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to print out an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+function derive(coefficient,exponent) {
+    return coefficient * exponent +'x^'+ (exponent - 1)
+  }
