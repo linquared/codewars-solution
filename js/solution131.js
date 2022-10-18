71f45142317e41c6ddc8d5ac2b5477054416aa8d
linsquared <lin2013lin@gmail.com>
2022-10-17 23:19:52 -0400

    131

---
+++ js/solution131.js
@@ -0,0 +1,9 @@
+// 8kyu- Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+function preFizz(n) {
+    let result = []
+    for(let i = 1; i <= n; i++){
+        result.push(i)
+    }
+    return result
+}
+
