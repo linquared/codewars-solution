8abefa0dcd67243c59acb97ae33e74cebb4b0e81
linsquared <lin2013lin@gmail.com>
2022-11-01 22:15:05 -0700

    147

---
+++ js/solution147.js
@@ -0,0 +1,5 @@
+// 8kyu- write a function that always return 5. Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+function unusualFive() {
+  const items = ['apple', 'pears','kiwi', 'tea', 'peach']
+  return items.length;
+}
