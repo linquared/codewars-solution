98d6d87d29d310b47de837806eb6864a03f7fa52
linsquared <lin2013lin@gmail.com>
2022-11-13 20:03:41 -0500

    158

---
+++ js/solution158.js
@@ -0,0 +1,5 @@
+// 7kyu-Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits.
+function digits(n) {
+    return n.toString().split('').length
+  }
+
