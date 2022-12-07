aec80f63b947aa86f46ffe6987579a3a26706f02
linsquared <lin2013lin@gmail.com>
2022-12-06 21:13:39 -0900

    183

---
+++ js/solution183.js
@@ -0,0 +1,4 @@
+// 6kyu - Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+var countBits = function(n) {
+  return n.toString(2).split('').filter(num => num ==1).length
+};
