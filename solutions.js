e267ced4635f8023bf03115e69c37bd7fd00a3f1
Lin Lin <lin2013lin@gmail.com>
2023-09-17 21:22:24 -1100

    225

--- solutions.js
+++ solutions.js
@@ -207,22 +207,23 @@ function sort(str) {
 
 // use selection sort to sort the arrary from small to large number
 
-const selectionSort = (arr) => {
+const selectionSort = (arr, target) => {
+
+  let count = 0
 
   for (let i = 0; i < arr.length; i++) {
-    let lowest = i
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[lowest]) {
-        lowest = j
-        let temp = arr[i]
-        arr[i] = arr[lowest]
-        arr[j] = temp
-      }
+    if (arr[i] !== arr[i + 1]) {
+      console.log('ay')
+    } else {
+      count++
     }
   }
-  return arr
-
+  return count - 1
 }
 
 
-console.log(selectionSort([22, 50, 16, 9, 2]))
+console.log(selectionSort([1, 0, 0, 0, 1, 0, 0, 1], 1))
+
+
+
+
