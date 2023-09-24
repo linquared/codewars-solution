837abd957c6ac512a23a88403df45691b7816747
Lin Lin <lin2013lin@gmail.com>
2023-09-23 22:23:43 -0400

    229

--- js/solution228.js
+++ js/solution228.js
@@ -1,3 +1,9 @@
+
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
 var groupAnagrams = function (strs) {
     let map = {}
 
@@ -6,5 +12,5 @@ var groupAnagrams = function (strs) {
         if (!map[s]) map[s] = []
         map[s].push(str)
     }
-    return Object.values(map)
+    return map
 };
