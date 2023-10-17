789d796bb52378ae8974c2ac84eed7a6e2e4954f
Lin Lin <lin2013lin@gmail.com>
2023-10-17 19:15:20 -0400

    better version of 235

--- js/solution235.js
+++ js/solution235.js
@@ -3,16 +3,21 @@
 // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 // Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit, return 0.
-
 const stock = (nums) => {
-    let temp = 0
     let profit = 0
+    let left = 0
+    let right = 1
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            temp = nums[i] - nums[j]
-            profit = Math.min(temp, profit)
+    while (right < nums.length) {
+
+        if (nums[left] < nums[right]) {
+            let currentProfit = nums[right] - nums[left]
+            profit = Math.max(currentProfit, profit)
+        } else {
+            left = right
         }
+        right++
+
     }
-    return Math.abs(profit)
+    return profit
 }
